a533a53b4727d1c4c51a0f14796d39b27a109bf0
MA Miller <miller.m.43@gmail.com>
2018-02-09 19:02:25 -0500

    More fields for models.

--- mlb_data_models.py
+++ mlb_data_models.py
@@ -16,6 +16,7 @@ class Team(BaseModel):
     division = TextField(null=True)
     team = IntegerField(db_column='team_id', null=True)
     team_name = TextField(null=True)
+    stadium_type = TextField(null=True)
 
     class Meta:
         db_table = 'teams'
@@ -25,6 +26,8 @@ class Game(BaseModel): #initialize with default values
     home_team = TextField(null=True)
     away_pts = IntegerField(null=True)
     home_pts = IntegerField(null=True)
+    away_pitcher_id = IntegerField(null=True)
+    home_pitcher_id = IntegerField(null=True)
 
     class Meta:
         db_table = 'games'
