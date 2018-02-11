4a2a72a61a8043596f0caaa6eca613a9bd9b4afc
MA Miller <miller.m.43@gmail.com>
2018-02-11 11:45:52 -0500

    Writing simulation script (combining all the subfunctions of the main-dot-py script into one), added sqlite database to repository

---
+++ james.py
@@ -0,0 +1,12 @@
+#Selection of mathematical models for MLB win predictions
+
+def runs_regress(dpts):
+  import math
+  return 1/(1+math.exp(-1*(0.15+dpts*0.2)))
+
+def SRS_regress(dSRS):
+  import math
+  return 1/(1+math.exp(-1*(0.15+dSRS*0.35)))
+
+if __name__=='__main__':
+  print(SRS_regress(2))
---
+++ mcss.py
@@ -0,0 +1,59 @@
+"""
+This script will implement the same strategy used in the "Naismith"  program,
+but only in one script.
+"""
+
+import datetime
+from mlb_data_models import Game
+from pprint import pprint
+from james import SRS_regress
+
+#Known wins - to be implemented, not yet required for obvious reasons.
+
+#Future games.
+x=Game.select().where(Game.scheduled_date>=datetime.datetime.now())
+#id,scheduled_date,away_team,home_team
+x=[[i.id,i.scheduled_date,i.away_team,i.home_team] for i in x]
+pprint(x)
+
+#Analytics component - get ratings (this is ptsaverages and ratings_calculations under the old scheme
+#TBD - for now use a dummy - these are the 2017 end of season ranks
+ratings=[{'abbreviation':'Ana','rating':0.1},
+{'abbreviation':'Ari','rating':0.8},
+{'abbreviation':'Atl','rating':-0.7},
+{'abbreviation':'Bal','rating':-0.3},
+{'abbreviation':'Bos','rating':0.8},
+{'abbreviation':'ChC','rating':0.6},
+{'abbreviation':'ChW','rating':-0.5},
+{'abbreviation':'Cin','rating':-0.7},
+{'abbreviation':'Cle','rating':1.5},
+{'abbreviation':'Col','rating':0.3},
+{'abbreviation':'Det','rating':-0.8},
+{'abbreviation':'Fla','rating':-0.5},
+{'abbreviation':'Hou','rating':1.2},
+{'abbreviation':'Kan','rating':-0.4},
+{'abbreviation':'Los','rating':0.9},
+{'abbreviation':'Mil','rating':0.1},
+{'abbreviation':'Min','rating':0.2},
+{'abbreviation':'NYM','rating':-0.9},
+{'abbreviation':'NYY','rating':1.3},
+{'abbreviation':'Oak','rating':-0.4},
+{'abbreviation':'Phi','rating':-0.7},
+{'abbreviation':'Pit','rating':-0.4},
+{'abbreviation':'Sdg','rating':-1.3},
+{'abbreviation':'Sea','rating':0.0},
+{'abbreviation':'Sfo','rating':-0.9},
+{'abbreviation':'StL','rating':0.2},
+{'abbreviation':'Tam','rating':0.1},
+{'abbreviation':'Tex','rating':0.0},
+{'abbreviation':'Tor','rating':-0.3},
+{'abbreviation':'Was','rating':0.6}]
+
+for i,x in enumerate(ratings):
+    x['team_id']=i+1
+
+#End of manual ratings.
+
+#
+
+
