9d8c461830851d69f6fb214bb57b2f1774c5e47a
MA Miller <miller.m.43@gmail.com>
2018-02-12 09:00:54 -0500

    Minor edits. Standings projections is now broken

--- mcss.py
+++ mcss.py
@@ -17,17 +17,23 @@ def mcss(game_dict_list):
     
 if __name__=='__main__':
     import json
+    import numpy as np
     from pprint import pprint
     from mlb_data_models import Team,database
 
     cursor=database.execute_sql('select distinct division from teams;')
     list_of_divisions=[row[0] for row in cursor]
 
-    for d in list_of_divisions:
-        division_teams=Team.select(Team.team_name,Team.id).where(Team.division==d).get()
-        pprint(division_teams)
+    division_teams=Team.select(Team.team_name,Team.id,Team.division)
+    division_teams=[dict(zip(['team_name','team_id','division'],[x.team_name,x.id,x.division])) for x in division_teams]
 
     with open('test_dicts','r') as fin:
         test_dict=json.load(fin)
 
-    pprint(test_dict)
+    win_matrix=mcss(test_dict)
+
+    total_wins=np.sum(win_matrix,axis=0)
+    for x in division_teams:
+        x['total_wins']=total_wins[x['team_id']-1]
+
+    pprint(division_teams)
