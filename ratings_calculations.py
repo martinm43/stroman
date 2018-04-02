7b3cadfe4279f1beef0e16b967f4e6b85a1d5f9e
MA Miller <miller.m.43@gmail.com>
2018-04-02 16:19:55 -0300

    added ratings calcs. need to reqd the api for more and proper completion

--- ratings_calculations.py
+++ ratings_calculations.py
@@ -1,7 +1,7 @@
 """
 Ratings_Calculations
 
-This script calculates the Burke ratings for NBA teams over a given time period with a given 
+This script calculates the Burke ratings for MLB teams over a given time period with a given 
 maximum/minimum margin of victory and average home team advantage for use in the projections
 scripts. 
 
@@ -19,7 +19,7 @@ import csv,os
 import time
 from string_conversion_tools import team_abbreviation
 import sqlite3
-from dbtools.access_nba_data import epochtime
+from dbtools.access_MLB_data import epochtime
 
 wkdir = os.path.dirname(os.path.realpath(__file__))+'/'
 
@@ -37,7 +37,7 @@ except ImportError:
 
 srsdata=[]
 
-filename='nba_data.sqlite'
+filename='MLB_data.sqlite'
 conn=sqlite3.connect(wkdir+filename)
 c=conn.cursor()
 
@@ -57,12 +57,14 @@ if burke_solve==1:
     max_MOV=20.0
     home_team_adv=2.0
 
-    nba_api_srsdata_query_str='SELECT away_team_id, away_PTS, home_team_id, home_PTS\
-                             from bballref_scores WHERE datetime >= '+str(analysis_start_date)+' AND datetime <= '+str(analysis_end_date)+' AND \
-                             away_pts>0'
-    nba_api_srsdata=c.execute(nba_api_srsdata_query_str).fetchall()
-    srsdata=nba_api_srsdata
-    burke_data=[[s[2],s[0],s[3],s[1]] for s in srsdata if s[1] is not None]
+#    nba_api_srsdata_query_str='SELECT away_team_id, away_PTS, home_team_id, home_PTS\
+#                             from bballref_scores WHERE datetime >= '+str(analysis_start_date)+' AND datetime <= '+str(analysis_end_date)+' AND \
+#                             away_pts>0'
+#    nba_api_srsdata=c.execute(nba_api_srsdata_query_str).fetchall()
+#    srsdata=nba_api_srsdata
+#    burke_data=[[s[2],s[0],s[3],s[1]] for s in srsdata if s[1] is not None]
+
+
     burkelist=burke_calc(burke_data,impmode=None,max_MOV=max_MOV,home_team_adv=home_team_adv,win_floor=6.0)
     burkelist=[[b] for b in burkelist]
 else:
