694a8475577b74d7c685d1c6d0084c4efd8404d2
MA Miller <miller.m.43@gmail.com>
2018-04-05 17:11:57 -0300

    Reindexing is complete

--- full_ratings_calculations.py
+++ full_ratings_calculations.py
@@ -7,7 +7,9 @@ maximum/minimum margin of victory and average home team advantage for use in the
 scripts. 
 
 """
+from __future__ import print_function, division
 
+from pprint import pprint
 import csv,os
 import time
 import numpy as np
@@ -40,24 +42,31 @@ games=[[g.away_team,g.away_runs,g.home_team,g.home_runs] for g in games]
 
 #Basic run-based calculations
 # Create a 30x9 vector: rows are teams, columns are:
-# . "WINS" - to be added
-# . "LOSSES" - to be added
-#1. "RUNS SCORED"
-#2. "RUNS ALLOWED"
-#3. "GAMES PLAYED"
-#4.  "AVERAGE RUNS SCORED"
-#5. "AVERAGE RUNS ALLOWED"
-#6. "AVERAGE RUN DIFFERENTIAL"
-#7. "RUN DIFFERENTIAL"
-#8. "PYTHAGOREAN_WINS"
-#9. "ADJUSTED RATING"
+#1. "WINS" - to be added
+#2. "LOSSES" - to be added
+#3. "RUNS SCORED"
+#4. "RUNS ALLOWED"
+#5. "GAMES PLAYED"
+#6.  "AVERAGE RUNS SCORED"
+#7. "AVERAGE RUNS ALLOWED"
+#8. "AVERAGE RUN DIFFERENTIAL"
+#9. "RUN DIFFERENTIAL"
+#10. "PYTHAGOREAN_WINS"
+#11. "ADJUSTED RATING"
 # 0 through 11
 
 #then prepend the team names and division names to the list for sorting
 
-diff_matrix=np.zeros((30,9))
+diff_matrix=np.zeros((30,11))
 for g in games:
-    #Away team.
+    #two operations required, to increase the net wins and losses for both teams in 
+    #each pass. Like if "away runs" greater than home runs then away team won. Advanced logic.
+    if g[1]>g[3]:
+        diff_matrix[g[0]-1,0]+=1
+        diff_matrix[g[2]-1,1]+=1
+    elif g[1]<g[3]:
+        diff_matrix[g[2]-1,0]+=1
+        diff_matrix[g[0]-1,1]+=1
     diff_matrix[g[0]-1,2]+=g[1]
     diff_matrix[g[0]-1,3]+=g[3]
     diff_matrix[g[0]-1,4]+=1
@@ -67,23 +76,23 @@ for g in games:
     diff_matrix[g[2]-1,4]+=1
 
 for i in range(0,len(diff_matrix)):
-    diff_matrix[i,5]=diff_matrix[i,0]/diff_matrix[i,2]
-    diff_matrix[i,6]=diff_matrix[i,1]/diff_matrix[i,2]
-    diff_matrix[i,7]=diff_matrix[i,3]-diff_matrix[i,4]
-    diff_matrix[i,8]=diff_matrix[i,5]*diff_matrix[i,2]
-    diff_matrix[i,9]=diff_matrix[i,0]**pythag_factor/(diff_matrix[i,0]**pythag_factor+diff_matrix[i,1]**pythag_factor)
+    diff_matrix[i,5]=diff_matrix[i,2]/diff_matrix[i,4]
+    diff_matrix[i,6]=diff_matrix[i,3]/diff_matrix[i,4]
+    diff_matrix[i,7]=diff_matrix[i,5]-diff_matrix[i,6]
+    diff_matrix[i,8]=diff_matrix[i,7]*diff_matrix[i,4]
+    diff_matrix[i,9]=diff_matrix[i,2]**pythag_factor/(diff_matrix[i,2]**pythag_factor+diff_matrix[i,3]**pythag_factor)
 
 #Adjusted Rating Calculations (Burke - after Brian Burke - ratings)
 burke_data=[[g[2],g[0],g[3],g[1]] for g in games]
 burkelist=burke_calc(burke_data,impmode=None,max_MOV=max_MOV,home_team_adv=home_team_adv,win_floor=0.0)
 burkelist=[[b] for b in burkelist]
-diff_matrix[:,8]=[b[0] for b in burkelist]
+diff_matrix[:,10]=[b[0] for b in burkelist]
 
 
 
 diff_list=diff_matrix.tolist()
 
-print(diff_list)
+pprint(diff_list)
 
 ######################
 # PRINTING TO SCREEN #
@@ -91,12 +100,12 @@ print(diff_list)
 
 print('Listing run differentials:')
 for i,x in enumerate(diff_list):
-    print('The '+id_to_mlbgames_name(i+1)+' have a run differential of '+'{0}'.format(x[6])+\
-          ', scoring '+'{0}'.format(x[0])+' runs while allowing '+\
-          '{0}'.format(x[1])+' runs.')
+    print('The '+id_to_mlbgames_name(i+1)+' have a run differential of '+'{0}'.format(x[8])+\
+          ', scoring '+'{0}'.format(x[2])+' runs while allowing '+\
+          '{0}'.format(x[3])+' runs.')
 print('Listing pythagorean win expectancies: ')
 for i,x in enumerate(diff_list):
-    print(id_to_mlbgames_name(i+1)+': '+'{:.1f}'.format(x[7]*162))
+    print(id_to_mlbgames_name(i+1)+': '+'{:.1f}'.format(x[9]*162))
 
 
 print('Printing Burke Ratings (ratings based on strength of schedule and any perceived home field advantage).')
@@ -115,5 +124,5 @@ for row in burkelist:
     csvwriter.writerow(row)
 csvfile_out.close()
 
-vector_of_means=[[x[6]] for x in diff_list]
+vector_of_means=[[x[8]] for x in diff_list]
 list_to_csv('run_diff_vector.csv',vector_of_means)
