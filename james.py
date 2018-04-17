cab0a68297b916c3e55fa398d15df4a22c1a930e
MA Miller <miller.m.43@gmail.com>
2018-04-16 20:53:10 -0400

    testing the calibration files

--- james.py
+++ james.py
@@ -14,7 +14,7 @@ def runs_regress(dpts):
 
 def SRS_regress(dSRS):
   import math
-  return 1/(1+math.exp(-1*(0.15+dSRS*0.35)))
+  return 1/(1+math.exp(-1*(0.15+dSRS*0.85)))
 
 if __name__=='__main__':
   print(SRS_regress(2))
