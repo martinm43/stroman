a2cc192771aae2fcad8ee33c49579e0101c606cd
MA Miller <miller.m.43@gmail.com>
2018-04-17 22:39:23 -0400

    edited standings projection file to remove the dummy ratings

--- standings_projection.py
+++ standings_projection.py
@@ -5,7 +5,6 @@ but only in one script.
 
 import csv
 import json
-import numpy as np
 import datetime
 from mlb_data_models import Game
 from pprint import pprint
@@ -21,36 +20,36 @@ game_dict_list=[dict(zip(['id','scheduled_date','away_team','home_team'],[i.id,i
 
 #Analytics component - get ratings (this is ptsaverages and ratings_calculations under the old scheme
 #TBD - for now use a dummy - these are the 2017 end of season ranks
-ratings=[{'abbreviation':'Ana','rating':0.1},
-{'abbreviation':'Ari','rating':0.8},
-{'abbreviation':'Atl','rating':-0.7},
-{'abbreviation':'Bal','rating':-0.3},
-{'abbreviation':'Bos','rating':0.8},
-{'abbreviation':'ChC','rating':0.6},
-{'abbreviation':'ChW','rating':-0.5},
-{'abbreviation':'Cin','rating':-0.7},
-{'abbreviation':'Cle','rating':1.5},
-{'abbreviation':'Col','rating':0.3},
-{'abbreviation':'Det','rating':-0.8},
-{'abbreviation':'Fla','rating':-0.5},
-{'abbreviation':'Hou','rating':1.2},
-{'abbreviation':'Kan','rating':-0.4},
-{'abbreviation':'Los','rating':0.9},
-{'abbreviation':'Mil','rating':0.1},
-{'abbreviation':'Min','rating':0.2},
-{'abbreviation':'NYM','rating':-0.9},
-{'abbreviation':'NYY','rating':1.3},
-{'abbreviation':'Oak','rating':-0.4},
-{'abbreviation':'Phi','rating':-0.7},
-{'abbreviation':'Pit','rating':-0.4},
-{'abbreviation':'Sdg','rating':-1.3},
-{'abbreviation':'Sea','rating':0.0},
-{'abbreviation':'Sfo','rating':-0.9},
-{'abbreviation':'StL','rating':0.2},
-{'abbreviation':'Tam','rating':0.1},
-{'abbreviation':'Tex','rating':0.0},
-{'abbreviation':'Tor','rating':0.7}, #edited, because I am a shameless homer (and because the first half hell season isn't representative)
-{'abbreviation':'Was','rating':0.6}]
+ratings=[{'abbreviation':'Ana'},
+{'abbreviation':'Ari'},
+{'abbreviation':'Atl'},
+{'abbreviation':'Bal'},
+{'abbreviation':'Bos'},
+{'abbreviation':'ChC'},
+{'abbreviation':'ChW'},
+{'abbreviation':'Cin'},
+{'abbreviation':'Cle'},
+{'abbreviation':'Col'},
+{'abbreviation':'Det'},
+{'abbreviation':'Fla'},
+{'abbreviation':'Hou'},
+{'abbreviation':'Kan'},
+{'abbreviation':'Los'},
+{'abbreviation':'Mil'},
+{'abbreviation':'Min'},
+{'abbreviation':'NYM'},
+{'abbreviation':'NYY'},
+{'abbreviation':'Oak'},
+{'abbreviation':'Phi'},
+{'abbreviation':'Pit'},
+{'abbreviation':'Sdg'},
+{'abbreviation':'Sea'},
+{'abbreviation':'Sfo'},
+{'abbreviation':'StL'},
+{'abbreviation':'Tam'},
+{'abbreviation':'Tex'},
+{'abbreviation':'Tor'},
+{'abbreviation':'Was'}]
 
 file_ratings=[]
 
