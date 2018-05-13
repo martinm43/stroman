02607346a4556c90b62f794b1bf9111a929bfb41
MA Miller <miller.m.43@gmail.com>
2018-05-13 17:40:27 -0400

    Added in new cpp files for study and development of new deliverable

--- armadillo_example.cpp
+++ armadillo_example.cpp
@@ -8,9 +8,9 @@ int main()
 
   // arma::endr represents the end of a row in a matrix
   arma::mat A;
-  A << 1.0 << 2.0 << arma::endr
-    << 2.0 << 3.0 << arma::endr
-    << 1.0 << 3.0 << arma::endr;
+  A << 1.0 << 1.0 << 2.0 << arma::endr
+    << 1.0 << 2.0 << 3.0 << arma::endr
+    << 1.0 << 1.0 << 3.0 << arma::endr;
 
   std::cout << "Least squares solution:\n";
   std::cout << solve(A,b) << '\n';
