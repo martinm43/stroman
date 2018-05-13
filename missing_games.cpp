02ba1367b27e3e800afa84bbe9dcd2b2f869d4c1
MA Miller <miller.m.43@gmail.com>
2018-05-13 13:57:30 -0400

    created first cpp file using SQL and C++ api in order to get data

--- missing_games.cpp
+++ missing_games.cpp
@@ -1,5 +1,9 @@
+ #include <string>
+ #include <iostream>
  #include <stdio.h>
  #include <sqlite3.h>
+
+ using namespace std;
  
  static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    int i;
@@ -14,18 +18,26 @@
    sqlite3 *db;
    char *zErrMsg = 0;
    int rc;
- 
+
+   //replacing argv[1]
+   string DatabaseName("mlb_data.sqlite");
+   //replacing argv[2]
+   string SQLStatement("SELECT * FROM POSTPHONED_GAMES_LOCAL"); 
+
+   /* No longer necessary as we're using arguments in code
    if( argc!=3 ){
      fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
      return(1);
    }
-   rc = sqlite3_open(argv[1], &db);
+   */
+
+   rc = sqlite3_open(DatabaseName.c_str(), &db);
    if( rc ){
      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
      sqlite3_close(db);
      return(1);
    }
-   rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+   rc = sqlite3_exec(db, SQLStatement.c_str(), callback, 0, &zErrMsg);
    if( rc!=SQLITE_OK ){
      fprintf(stderr, "SQL error: %s\n", zErrMsg);
      sqlite3_free(zErrMsg);
