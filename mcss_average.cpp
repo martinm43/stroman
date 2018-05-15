8d987f5bea68630cd394d58d19f74c9fe174b136
MA Miller <miller.m.43@gmail.com>
2018-05-15 17:32:53 -0400

    stored values successfully in mcss_average array

--- mcss_average.cpp
+++ mcss_average.cpp
@@ -9,7 +9,17 @@
 
 using namespace std;
 
-//class Game(int away_team, int away_runs, int home_team, int home_runs)
+class Game{
+private:
+
+   int _away_team_id;
+   int _away_runs;
+   int _home_team_id;
+   int _home_runs;
+
+public:
+   Game(int away_team_id, int away_runs, int home_team_id, int home_runs): _away_team_id(away_team_id), _away_runs(away_runs), _home_team_id(home_team_id), _home_runs(home_runs) {}
+};
 
 int main()
 {
@@ -17,10 +27,9 @@ int main()
     char *zErrMsg = 0;
     int rc;
 
-    //For what we choose to store the values in.
-    //vector<Address> games;
-    
+    vector<Game> games;
 
+    //SQLite connection stuff
     string DatabaseName("mlb_data.sqlite");
 
     string SQLStatement("SELECT away_team, away_runs, home_team, home_runs "
@@ -40,24 +49,33 @@ int main()
                             -1, &stmt, NULL);
     if (rc != SQLITE_OK) {
         cerr << "SELECT failed: " << sqlite3_errmsg(db) << endl;
+        sqlite3_finalize(stmt);
         return 1;
     }
+
     while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-        //int id = sqlite3_column_int(stmt, 0);
-        //const char* name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
-        //const char* number = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
-        // let's assume number can be NULL:
-        //addresses.push_back(Address(id, name, number ? number : ""));
+        //Writing out the games to the screen (temporary for now, just for error checking)
         cout << "Away team id: " << sqlite3_column_int(stmt,1) << endl;
         cout << "Away team runs scored: " << sqlite3_column_int(stmt,2) << endl;
         cout << "Home team id: "<< sqlite3_column_int(stmt,3) << endl;
         cout << "Home team runs scored: " << sqlite3_column_int(stmt,4) << endl;
+
+        //Store in our vector
+        int away_team_id = sqlite3_column_int(stmt,1);
+        int away_runs = sqlite3_column_int(stmt,2);
+        int home_team_id = sqlite3_column_int(stmt,3);
+        int home_runs = sqlite3_column_int(stmt,4);
+        games.push_back(Game(away_team_id,away_runs,home_team_id,home_runs));
     }
     if (rc != SQLITE_DONE) {
         cerr << "SELECT failed: " << sqlite3_errmsg(db) << endl;
         // if you return/throw here, don't forget the finalize
+        sqlite3_finalize(stmt);
+        return 1;   
     }
     sqlite3_finalize(stmt);
+ 
+    cout<<games[0]<<endl;
 
 return 0;
 }
