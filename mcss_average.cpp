83f6f6def13b13e56bf691e2cd799f4a603ec3ee
MA Miller <miller.m.43@gmail.com>
2018-05-15 22:09:20 -0400

    added in classes for games and teams, but still have not been able to get the 'creator' to work for the Teams vector

--- mcss_average.cpp
+++ mcss_average.cpp
@@ -18,7 +18,40 @@ private:
    int _home_runs;
 
 public:
-   Game(int away_team_id, int away_runs, int home_team_id, int home_runs): _away_team_id(away_team_id), _away_runs(away_runs), _home_team_id(home_team_id), _home_runs(home_runs) {}
+   Game(int away_team_id, int away_runs, int home_team_id, int home_runs): 
+     _away_team_id(away_team_id), _away_runs(away_runs), _home_team_id(home_team_id), _home_runs(home_runs) {}
+
+   int get_away_team_id() const {return _away_team_id;}
+   int get_away_runs() const {return _away_runs;}
+   int get_home_team_id() const {return _home_team_id;}
+   int get_home_runs() const {return _home_runs;}
+
+};
+
+class Team{
+private:
+
+    int _team_id;
+    string _team_name;
+    string _mlbgames_name;
+    string _abbreviation;
+    string _division;
+    string _league;
+
+public:
+
+    Team(int team_id, string team_name, string mlbgames_name, 
+            string abbreviation, string division, string league):
+        _team_id(team_id), _team_name(team_name), _mlbgames_name(mlbgames_name), 
+            _abbreviation(abbreviation), _division(division), _league(league) {}
+
+    int get_team_id() const {return _team_id;}
+    string get_team_name() const {return _team_name;}
+    string get_mlbgames_name() const {return _mlbgames_name;}
+    string get_abbreviation() const {return _abbreviation;}
+    string get_division() const {return _division;}
+    string get_league() const {return _league;}
+
 };
 
 int main()
@@ -28,6 +61,7 @@ int main()
     int rc;
 
     vector<Game> games;
+    vector<Team> teams;
 
     //SQLite connection stuff
     string DatabaseName("mlb_data.sqlite");
@@ -69,13 +103,56 @@ int main()
     }
     if (rc != SQLITE_DONE) {
         cerr << "SELECT failed: " << sqlite3_errmsg(db) << endl;
-        // if you return/throw here, don't forget the finalize
         sqlite3_finalize(stmt);
         return 1;   
     }
     sqlite3_finalize(stmt);
  
-    cout<<games[0]<<endl;
+    cout<<games[0].get_away_team_id()<<endl;
+
+    /* 
+    Statement number two, initializing the list of teams 
+    */
+
+    SQLStatement = "SELECT team_id, team_name, mlbgames_name, abbreviation, division, league from teams;";
+
+
+    rc = sqlite3_prepare_v2(db, SQLStatement.c_str(),
+                            -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        cerr << "SELECT failed: " << sqlite3_errmsg(db) << endl;
+        sqlite3_finalize(stmt);
+        return 1;
+    }
+
+    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        //Store in our vector
+
+         cout << sqlite3_column_text(stmt,2) << endl;
+         cout << sqlite3_column_text(stmt,3) << endl;
+         cout << sqlite3_column_text(stmt,4) << endl;
+         cout << sqlite3_column_text(stmt,5) << endl;
+         cout << sqlite3_column_text(stmt,6) << endl;
+
+        /* - compiles but causes a CORE DUMP. DUMP THE CORE CLIP.
+        int team_id = sqlite3_column_int(stmt,1);
+        string team_name = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt,2)));
+        string mlbgames_name = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt,3)));
+        string abbreviation = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt,4)));
+        string division = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt,5)));
+        string league = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt,6)));
+        teams.push_back(Team(team_id,team_name,mlbgames_name,abbreviation,division,league));
+        */
+
+    }
+    if (rc != SQLITE_DONE) {
+        cerr << "SELECT failed: " << sqlite3_errmsg(db) << endl;
+        sqlite3_finalize(stmt);
+        return 1;   
+    }
+    sqlite3_finalize(stmt);
+
+
 
 return 0;
 }
