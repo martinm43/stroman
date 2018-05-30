803f06e65007a3ec846e208d39c539d9a9d2643a
MA Miller <miller.m.43@gmail.com>
2018-05-30 06:57:01 -0400

    minor edits to remove erroneous lines

--- pandas_test.py
+++ pandas_test.py
@@ -3,7 +3,5 @@ import pandas as pd
 import sqlite3
 conn=sqlite3
 conn=sqlite3.connect("mlb_data.sqlite")
-df=pd.read_sql_query("select * from games where not (away_runs=0 and home_runs=0) and scheduled_date < datetime('now')")
 df=pd.read_sql_query("select * from games where not (away_runs=0 and home_runs=0) and scheduled_date < datetime('now');",conn)
-df
 df
