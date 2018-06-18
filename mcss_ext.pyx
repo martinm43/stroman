c955e8490a36471bb197ff4634c4ca0b9ae4e0c6
MA Miller <miller.m.43@gmail.com>
2018-06-17 23:18:57 -0400

    Adding necessary cython file.

---
+++ mcss_ext.pyx
@@ -0,0 +1,12 @@
+# distutils: language = c++
+# distutils: sources = mcss.cpp
+
+# Cython interface file for wrapping the object
+#
+#
+
+from libcpp.vector cimport vector
+
+# c++ interface to cython
+cdef extern from "mcss.h":
+    vector[vector[double]] simulations_results_vectorized()
