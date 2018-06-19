3179bb2d7b9da828a08cd094f8949247131c3bdb
MA Miller <miller.m.43@gmail.com>
2018-06-19 08:02:05 -0400

    The extension and the .pyx file must have the same name. This is not an error on certain compilers but is an error for gcc based linking. Error fixed on Ubuntu 16.04 LTS

--- cpp_setup.py
+++ cpp_setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 # Use python setup.py build_ext --inplace
 # to compile
 
-ext = Extension("mcss",sources=["mcss_ext2.pyx","mcss.cpp"])
+ext = Extension("mcss_ext2",sources=["mcss_ext2.pyx","mcss.cpp"])
 
 setup(
   name = "cython_mcss",
