7184f2794ae7afa7ebdc18cebdf00e4700facfc6
MA Miller <miller.m.43@gmail.com>
2019-05-11 18:44:43 +0000

    adding cron update file

---
+++ cron_update.py
@@ -0,0 +1,105 @@
+"""
+Gets data from the mlbgame api
+and then updates the existing db entries
+
+Need to doublecheck/tune algo
+"""
+
+
+
+import sys
+from datetime import datetime, timedelta
+
+import mlbgame
+from supports import teams_index_matcher
+from mlb_data_models import Game, Team
+from pprint import pprint
+
+end_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
+start_date = end_date - timedelta(days=2)
+
+game_d = start_date
+
+while game_d <= end_date:
+
+    print("Getting games from " + game_d.strftime("%Y-%m-%d"))
+
+    month = mlbgame.games(game_d.year, game_d.month, game_d.day)
+    games = mlbgame.combine_games(month)
+
+    game_list = []
+
+    # Following segment is based on the solution
+    # provided by Trevor V in the Github.
+    # Tries to see if a game has data available, processes it.
+    # If not, returns an error and the unique "mlbid" of the game
+    # that has ostensibly been PPD.
+
+    for game in games:
+        try:
+            home_team = game.home_team
+            away_team = game.away_team
+            print('Processing {0} ({1}) at {2} ({3})'.
+                  format(away_team, game.away_team_runs, #pylint:disable=no-member
+                         home_team, game.home_team_runs)) #pylint:disable=no-member
+            game_dict = {'mlbgame_away_team_name': away_team,
+                         'away_runs': game.away_team_runs, #pylint:disable=no-member
+                         'mlbgame_home_team_name': home_team,
+                         'home_runs': game.home_team_runs, #pylint:disable=no-member
+                         'mlbgame_id_str': game.game_id,
+                         'scheduled_date': game_d.strftime('%Y-%m-%d')}
+            game_list.append(game_dict)
+        except ValueError:
+            print('Unable to find data for game_id: {0}'.format(game.game_id))
+
+    # adhoc team processing
+    teams_index = Team.select(Team.id, Team.mlbgames_name).execute() #pylint:disable=no-value-for-parameter
+    teams_index = [{'team_id': t.id, 'mlbgames_name': t.mlbgames_name}
+                   for t in teams_index]
+
+    for g in game_list:
+        g['away_team'] = teams_index_matcher(
+            teams_index, g['mlbgame_away_team_name'])
+        g['home_team'] = teams_index_matcher(
+            teams_index, g['mlbgame_home_team_name'])
+
+    print('Processing complete. Adding games into database')
+
+    for g in game_list:
+        game_scheduled_date = datetime.strptime(
+            g['scheduled_date'], '%Y-%m-%d')
+        game_season_year = game_scheduled_date.year
+        #Code to determine if the data entered already exists in database and 
+        #not duplicate it
+        existing_query = Game.select().where(Game.away_team == g['away_team'],
+                            Game.home_team == g['home_team'],
+                            Game.scheduled_date == game_scheduled_date,
+                            Game.away_runs == g['away_runs'],
+                            Game.home_runs == g['home_runs'])
+        existing_query_results_sample = [g for g in existing_query]
+        #print(len(existing_query_results_sample)) - debug print
+        if len(existing_query_results_sample) > 0: #entry already exists
+            print('Entry for '+g['mlbgame_away_team_name']+\
+                    ' at '+g['mlbgame_home_team_name']+' on '+\
+                    g['scheduled_date']+' exists in database, updating entry')
+            Game.update(
+                away_runs=g['away_runs'],
+                home_runs=g['home_runs']).where(
+                    Game.scheduled_date == game_scheduled_date,
+                    Game.is_postphoned == 0,
+                Game.away_team == g['away_team'],
+                Game.home_team == g['home_team']).execute() 
+        else:
+            Game.replace(
+                away_runs = g['away_runs'],
+                home_runs = g['home_runs'],
+                scheduled_date = game_scheduled_date,
+                away_team = g['away_team'],
+                away_team_name = g['mlbgame_away_team_name'],
+                home_team = g['home_team'],
+                home_team_name = g['mlbgame_home_team_name'],
+                season_year = game_season_year).execute() 
+
+        # "replace" is the upsert functionality of peewee  
+
+    game_d = game_d + timedelta(days=1)
