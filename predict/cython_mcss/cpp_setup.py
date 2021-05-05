d5f7bc5183c571e80dec464875c4478eaab9a583
MA Miller <miller.m.43@gmail.com>
2021-05-05 22:32:05 +0000

    Minor setup changes made

--- predict/cython_mcss/cpp_setup.py
+++ predict/cython_mcss/cpp_setup.py
@@ -7,17 +7,23 @@ as described in the mcss_ext2.pyx file.
 try:
     from setuptools import setup
     from setuptools import Extension
+    print("Using setuptools")
 except:
     from distutils.core import setup, Extension
-from Cython.Build import cythonize
+    print("Using disutils")
 
+from Cython.Build import cythonize
+import os
 # Use python setup.py build_ext --inplace
 # to compile
+
+# os.environ["CC"] = "clang++"
 
 ext = Extension(
     "mcss_ext2",
     sources=["mcss_ext2.pyx", "mcss.cpp"],
     extra_compile_args=["-std=c++11"],
+
 )
 
 setup(name="cython_mcss", ext_modules=cythonize(ext, language_level="3"))
