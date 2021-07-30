76d845b6328feb34c6b517a5f4df37d516b0a0e0
MA Miller <miller.m.43@gmail.com>
2021-07-30 00:09:43 -0400

    Jul 29 update

--- src/prediction_table.py
+++ src/prediction_table.py
@@ -247,7 +247,7 @@ if __name__ == "__main__":
     end_datetime = datetime.today()-timedelta(days=1) # a few weeks or months in
     # in-season option: end_datetime = datetime.today()-timedelta(days=1)
 
-    ratings_mode = "SRS"
+    ratings_mode = "Elo"
     print("Working mode is "+ratings_mode)
     results = playoff_odds_calc(
         start_datetime, end_datetime, season_year, ratings_mode=ratings_mode)
