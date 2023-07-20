db5c8a6310d68accc992c01ca78b7d51f7f98c90
MA Miller <miller.m.43@gmail.com>
2023-07-19 23:20:06 -0400

    Master now uses new elo similar to old elo

--- src/info_table.py
+++ src/info_table.py
@@ -34,9 +34,9 @@ from random import randint
 from analytics.wins_script import get_wins
 
 # Query Testing
-season_year = 1986
+season_year = 2023
 start_datetime = datetime(season_year,3,15)
-end_datetime = datetime(season_year,10,15)
+end_datetime = datetime.today()
 
 games_list = games_query(start_datetime, end_datetime)
 
