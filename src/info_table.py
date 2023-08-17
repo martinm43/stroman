e6452fc06f110892813978cf5a89bc4378f51ff6
MA Miller <miller.m.43@gmail.com>
2023-08-17 16:44:04 -0400

    Calibrated solver, refining cpp SRS code

--- src/info_table.py
+++ src/info_table.py
@@ -34,7 +34,7 @@ from random import randint
 from analytics.wins_script import get_wins
 
 # Query Testing
-season_year = 2012
+season_year = 2011
 start_datetime = datetime(season_year,3,15)
 end_datetime = datetime(season_year,11,5)
 
