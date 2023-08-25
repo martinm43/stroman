d74d69b1c2f7818d273ac1d0dc71404e660231ac
MA Miller <miller.m.43@gmail.com>
2023-08-24 22:12:28 -0400

    removing commented out blocks

--- src/plot_season_odds.py
+++ src/plot_season_odds.py
@@ -30,7 +30,7 @@ from mlb_database.mlb_models import Teams
 year_max = 2023
 year_min = 1977
 
-""" try:
+try:
     season_year = int(input(f"Please select a year between {year_min} and {year_max}, or enter 0 for random year: "))
 except ValueError:
     print("Invalid value entered, quitting!!")
@@ -69,10 +69,10 @@ try:
     print(division_name)
 except KeyError:
     print("Invalid division key provided, quitting")
-    sys.exit(1) """
+    sys.exit(1)
 
-season_year = 2023
-division_name = "NL Central"
+#season_year = 2023
+#division_name = "NL Central"
 ratings_mode = "SRS"
 
 a = datetime(season_year, 3, 20)
