fe1dde3e5769a11d3b21183dc85a41479d1292e8
MA Miller <miller.m.43@gmail.com>
2023-08-29 21:18:17 -0400

    multi platform script complete

--- src/build_srs_script.sh
+++ src/build_srs_script.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
-g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/local/include/eigen3 -O3 #macOS version.
-g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/include/eigen3 -O3 #Linux version.
+#!/bin/bash
+
+# Check if the operating system is macOS
+if [[ $(uname) == "Darwin" ]]; then
+    echo "Compiling on macOS"
+    # Add macOS-specific commands here
+    g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/local/include/eigen3 -O3 #macOS version.
+fi
+
+# Check if the operating system is Linux/Unix
+if [[ $(uname) == "Linux" || $(uname) == "GNU" ]]; then
+    echo "Compiling on Linux/Unix"
+    # Add Linux/Unix-specific commands here
+    g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/include/eigen3 -O3 #Linux version.
+fi
+
+
