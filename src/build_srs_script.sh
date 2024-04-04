19fc6eccc7cfc20b45ab148a2e160b8eae44854c
MA Miller <miller.m.43@gmail.com>
2024-04-04 18:47:23 -0400

    adjusting build script to take termux into account

--- src/build_srs_script.sh
+++ src/build_srs_script.sh
@@ -7,8 +7,13 @@ if [[ $(uname) == "Darwin" ]]; then
     g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/local/include/eigen3 -O3 #macOS version.
 fi
 
-# Check if the operating system is Linux/Unix
-if [[ $(uname) == "Linux" || $(uname) == "GNU" ]]; then
+
+# Check if the operating system is Termux
+if [[ -n "$PREFIX" ]]; then
+    echo "Compiling on Termux"
+    # Add Linux/Unix-specific commands here
+    g++ srs.cpp -o SRSCalc -lsqlite3 -I $PREFIX/include/eigen3 -O3 #Linux version.
+elif [[ $(uname) == "Linux" || $(uname) == "GNU" ]]; then
     echo "Compiling on Linux/Unix"
     # Add Linux/Unix-specific commands here
     g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/include/eigen3 -O3 #Linux version.
