67e6846614fbf64af95f380e9fa657294939ece4
MA Miller <miller.m.43@gmail.com>
2025-07-20 15:22:25 -0400

    Post all star break initial data load

--- src/srs.cpp
+++ src/srs.cpp
@@ -58,7 +58,7 @@ int main() {
     std::vector<Rating> ratings_history;
 
     int start_year = 2022;
-    int end_year = 2024;
+    int end_year = 2025;
 
     // Set up times - start time.
     std::tm timeinfo = {}; // Initialize to all zeros
