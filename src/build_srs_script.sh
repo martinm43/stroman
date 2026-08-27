b86140ab9f4062c6a34f246a81c60e39d26efe23
MA Miller <miller.m.43@gmail.com>
2026-08-27 16:18:24 -0400

    Rebaselining the stroman code

--- src/build_srs_script.sh
+++ src/build_srs_script.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-# Check if the operating system is macOS
-if [[ $(uname) == "Darwin" ]]; then
-    echo "Compiling on macOS"
-    # Add macOS-specific commands here
-    g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/local/include/eigen3 -O3 #macOS version.
-fi
-
-
-# Check if the operating system is Termux
-if [[ -n "$PREFIX" ]]; then
-    echo "Compiling on Termux"
-    # Add Linux/Unix-specific commands here
-    g++ srs.cpp -o SRSCalc -lsqlite3 -I $PREFIX/include/eigen3 -O3 #Linux version.
-elif [[ $(uname) == "Linux" || $(uname) == "GNU" ]]; then
-    echo "Compiling on Linux/Unix"
-    # Add Linux/Unix-specific commands here
-    g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/include/eigen3 -O3 #Linux version.
-fi
+g++ srs.cpp -o SRSCalc -lsqlite3 -I /usr/include/eigen3 -O3 #Linux version.
 
 
